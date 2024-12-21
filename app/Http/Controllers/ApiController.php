f8c5854f948f0870d090fdee8e23147e41729748
naif-saleh <naif.saleh.code@gmail.com>
2024-12-21 15:12:29 +0200

    make auto dailer csv file validation

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -94,7 +94,7 @@ class ApiController extends Controller
     $response = Http::asForm()->post(config('services.three_cx.api_url') . '/connect/token', [
         'grant_type' => 'client_credentials',
         'client_id' => 'testapi',
-        'client_secret' => '1Y1PCtBfuS3BLJ2X4QCfhN1J2TBavZyd',
+        'client_secret' => 'ErWDuQZycBu6N6H5QYqQe9wUtPmfPHyw',
     ]);
 
     if ($response->failed()) {
