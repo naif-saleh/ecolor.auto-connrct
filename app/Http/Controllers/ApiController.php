3aca673d5ce5069e3babb24b1df030b087dd1cfc
naif-saleh <naif.saleh.code@gmail.com>
2024-12-25 12:06:20 +0200

    working on report

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -299,7 +299,7 @@ class ApiController extends Controller
             'client_id' => 'testapi',
             'client_secret' => '95ULDtdTRRJhJBZCp94K6Gd1BKRuaP1k',
         ]);
-        dd($response);
+        dd($response->body());
 
         if ($response->failed()) {
             return response()->json([
