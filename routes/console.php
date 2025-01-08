1d5264ae30144dff8ccb21bb09cd91a9a08a3366
naif-saleh <naif.saleh.code@gmail.com>
2025-01-08 16:54:49 +0200

    new updates

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// Schedule::command('app:make-call-command')->everySecond();
-// Schedule::command('app:participants-command')->everySecond();
+Schedule::command('app:make-call-command')->everySecond();
+Schedule::command('app:participants-command')->everySecond();
 Schedule::command('app:make-user-call-command')->everySecond();
 Schedule::command('app:make-user-participant-command')->everySecond();
 Schedule::command('app:update-user-status-command')->everySecond();
