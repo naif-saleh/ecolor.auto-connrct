e5626c5ad383b3fc6858a4207d2846da9798b765
naif-saleh <naif.saleh.code@gmail.com>
2025-01-16 15:43:09 +0200

    queuing calls

--- app/Console/Commands/makeCallCommand.php
+++ app/Console/Commands/makeCallCommand.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 use App\Jobs\MakeCallJob;
 use App\Services\TokenService;
-use Illuminate\Support\Facades\Queue;
+
 
 class makeCallCommand extends Command
 {
@@ -49,38 +49,66 @@ class makeCallCommand extends Command
     {
         $token = $this->tokenService->getToken();
         Log::info('MakeCallCommand executed at ' . now());
-        $autoDailerFiles = AutoDailerUploadedData::where('state', 'new')->orderBy('created_at', 'desc')->get();
-
+        $autoDailerFiles = AutoDailerUploadedData::where('state', 'new')->get();
 
         $now = Carbon::now();
 
         foreach ($autoDailerFiles as $feed) {
-            Log::info('count of calls in each time: ' . $autoDailerFiles->count());
-            // Calculate the time range by subtracting 3 hours from the given time
             $from = Carbon::createFromFormat('Y-m-d H:i:s', $feed->date . ' ' . $feed->from)->subHour(3);
             $to = Carbon::createFromFormat('Y-m-d H:i:s', $feed->date . ' ' . $feed->to)->subHour(3);
-
-            // Check if the current time is within the range for this file_id
-            Log::info('Current time: ' . $now);
-            Log::info('Time range for file ID ' . $feed->file->id . ' From: ' . $from . ' To: ' . $to);
-
+            Log::info('From: ' . $from . "To: ".$to);
             if ($now > $from && $now < $to && $feed->file->allow == 1) {
-                // The time is valid for this file_id, proceed with processing
                 Log::info('Processing file with ID ' . $feed->file->id);
 
-                
+                // $providerFeeds = AutoDailerUploadedData::where('file_id', $feed->file->id)
+                //     ->where('state', 'new')
+                //     ->get();
 
 
-                // After processing all provider feeds, mark the file as done if all numbers are called
+                    try {
+                        // Make the call
+                        $responseState = Http::withHeaders([
+                            'Authorization' => 'Bearer ' . $token,
+                        ])->post(config('services.three_cx.api_url') . "/callcontrol/{$feed->extension}/makecall", [
+                            'destination' => $feed->mobile,
+                        ]);
+
+                        // Wait for the response before proceeding to the next call
+                        if ($responseState->successful()) {
+                            $responseData = $responseState->json();
+                            Log::info('Call Response: ' . print_r($responseData));
+                            // Update or create report
+                            AutoDailerReport::firstOrCreate([
+                                'call_id' => $responseData['result']['callid'],
+                                'status' => $responseData['result']['status'],
+                                'provider' => $feed->provider,
+                                'extension' => $responseData['result']['dn'],
+                                'phone_number' => $responseData['result']['party_caller_id'],
+                            ]);
+
+                            // Update the status for the current mobile
+                            $feed->update([
+                                'state' => $responseData['result']['status'],
+                                'call_date' => Carbon::now(),
+                                'call_id' => $responseData['result']['callid'],
+                            ]);
+
+                            Log::info('Call successfully made for mobile ' . $feed->mobile);
+                        } else {
+                            Log::error('Failed to make call for mobile ' . $feed->mobile . '. Response: ' . $responseState->body());
+                        }
+                    } catch (\Exception $e) {
+                        Log::error('An error occurred: ' . $e->getMessage());
+                    }
+
+
                 $allCalled = AutoDailerUploadedData::where('file_id', $feed->file->id)->where('state', 'new')->count() == 0;
                 if ($allCalled) {
                     $feed->file->update(['is_done' => true]);
                     Log::info('All numbers in file ' . $feed->file->slug . ' have been called. The file is marked as done.');
                 }
             } else {
-                // If the time is not valid, skip the file and log it
                 Log::info('The current time is not within the specified range for file ID ' . $feed->file->id);
-                continue; // Skip this file and continue with the next one
             }
         }
     }
