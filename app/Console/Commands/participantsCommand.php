54087432653301ee8f06ef961bce6b7242fa17d0
Abdullah <bamlhs@gmail.com>
2025-01-18 18:57:54 +0300

    fix token

--- app/Console/Commands/participantsCommand.php
+++ app/Console/Commands/participantsCommand.php
@@ -46,7 +46,7 @@ class participantsCommand extends Command
 
        // $token = Cache::get('three_cx_token');
        $token = $this->tokenService->getToken();
-       Log::error("tokenServices: participantsCommand" . $token );
+       Log::info("tokenServices: participantsCommand" . $token );
 
         $providersFeeds = AutoDailerUploadedData::whereDate('created_at', Carbon::today())->get();
 
