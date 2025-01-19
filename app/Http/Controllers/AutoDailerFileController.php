e704bec79d988355bf01674bb2aee75d6655e8eb
naif-saleh <naif.saleh.code@gmail.com>
2025-01-19 09:58:12 +0200

    queuing calls

--- app/Http/Controllers/AutoDailerFileController.php
+++ app/Http/Controllers/AutoDailerFileController.php
@@ -113,7 +113,7 @@ class AutoDailerFileController extends Controller
             'to' => $request->to,
             'date' => $request->date,
         ];
-        $count = AutoDailerUploadedData::where('file_id', $id)->count();
+        $count = AutoDailerUploadedData::count();
         dd($count);
         // Process updates in batches of 5000 rows
         AutoDailerUploadedData::where('file_id', $id)->chunkById(5000, function ($records) use ($updateData) {
