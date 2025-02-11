0b8f489c99a689c5309f7dea41c762945dca0c55
root <root@vmi1682096.contaboserver.net>
2025-02-11 13:38:05 +0100

    scripts/make_call.sh

--- scripts/make_call.sh
+++ scripts/make_call.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
 while true
 do
 
 # Call Commands
 php8.4 /var/www/html/autoconnect/artisan app:ADial-make-call-command
-php8.4 /var/www/html/autoconnect/artisan app:ADist-make-call-command
 
 # Call Status Commands
 php8.4 /var/www/html/autoconnect/artisan app:ADial-participants-command
-# php8.4 /var/www/html/autoconnect/artisan app:ADist-participants-command
+php8.4 /var/www/html/autoconnect/artisan app:ADist-participants-command
 
 # Update User Status Commands
 php8.4 /var/www/html/autoconnect/artisan app:ADist-update-user-status-command
