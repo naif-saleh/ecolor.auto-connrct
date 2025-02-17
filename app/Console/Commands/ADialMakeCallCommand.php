8be60cae2b54bbe6525204e311db1805dee60eb9
naif-saleh <naif.saleh.code@gmail.com>
2025-02-17 17:20:37 +0200

    remove job

--- app/Console/Commands/ADialMakeCallCommand.php
+++ app/Console/Commands/ADialMakeCallCommand.php
@@ -103,7 +103,7 @@ class ADialMakeCallCommand extends Command
 
                     ADialData::where('feed_id', $file->id)
                         ->where('state', 'new')
-                        ->chunk((int)$callCount, function ($feed_data) use ($provider, $client) {
+                        ->chunk(50, function ($feed_data) use ($provider, $client) {
                             foreach ($feed_data as $data) {
                                 try {
                                     Log::info("ADIAL EXT: " . $provider->extension . " mobile: " . $data->mobile);
