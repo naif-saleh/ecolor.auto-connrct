d66b9d6e53820ae0c12e52dee8e5885fd25be2d5
naif-saleh <naif.saleh.code@gmail.com>
2025-02-27 17:45:16 +0200

    add

--- app/Console/Commands/ADistMakeCallCommand.php
+++ app/Console/Commands/ADistMakeCallCommand.php
@@ -100,12 +100,13 @@ class ADistMakeCallCommand extends Command
                         continue;
                     }
 
-
+Log::info('Today: ',today());
                     // ✅ Fetch Feeds
                     $feeds = ADistFeed::where('agent_id', $agent->id)
                         ->where('allow', true)
                         ->where('is_done',false)
                         ->where('date', today())
+                        ->orderBy('created_at', 'desc')
                         ->get();
 
                     Log::info("Fetched Feeds for Agent {$agent->id}: " . print_r($feeds->toArray(), true));
