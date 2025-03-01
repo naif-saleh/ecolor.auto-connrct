d9717efa14a02e435838683e47f5ba75c695b527
naif-saleh <naif.saleh.code@gmail.com>
2025-03-01 13:45:35 +0200

    add 3cx service class to optimize

--- app/Models/TemporaryCall.php
+++ app/Models/TemporaryCall.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TemporaryCall extends Model
 {
-    //
+
+    protected $fillable = [
+        'call_id',
+        'provider',
+        'extension',
+        'phone_number',
+        'call_data',
+        'status'
+    ];
+    
 }
