79e146c0955f96c6d580fff396471af0d4ef57b3
naif-saleh <naif.saleh.code@gmail.com>
2025-03-01 14:19:02 +0200

    add 3cx service class to optimize

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\General_Setting;
 use App\Models\CountCalls;
+use app\Models\ActivityLog;
+use Illuminate\Support\Facades\Auth;
 
 class SettingsController extends Controller
 {
@@ -26,11 +28,29 @@ class SettingsController extends Controller
             'call_time_end' => 'required|date_format:H:i',
         ]);
 
+        // Retrieve current values
+        $timeStart = General_Setting::get('call_time_start');
+        $timeEnd = General_Setting::get('call_time_end');
+
+        $comment = "Update Main Time from (Start) $timeStart to $request->call_time_start
+                    and (End) from $timeEnd to $request->call_time_end";
+
+        // Update settings
         General_Setting::set('call_time_start', $request->call_time_start . ':00', 'Start time for allowed calls');
         General_Setting::set('call_time_end', $request->call_time_end . ':00', 'End time for allowed calls');
 
+        // Log the operation
+        ActivityLog::create([
+            'user_id' => Auth::id(),
+            'operation' => $comment,
+            'file_type' => 'Main-Time',
+            'file_name' => 'Main-Time',
+            'operation_time' => now(),
+        ]);
+
         return redirect()->back()->with('success_time', 'Call time settings updated successfully.');
     }
+
 
 
     public function indexCountCall()
@@ -46,7 +66,18 @@ class SettingsController extends Controller
             'number_calls' => 'required',
         ]);
 
+        $count = CountCalls::get('number_calls');
         CountCalls::set('number_calls', $request->number_calls, 'Number of Calls Each Time');
+        $comment = "Update Number of Calls in moment from $count to $request->number_calls";
+
+         // Log the operation
+         ActivityLog::create([
+            'user_id' => Auth::id(),
+            'operation' => $comment,
+            'file_type' => 'Number of Calls in moment',
+            'file_name' => 'Number of Calls in moment',
+            'operation_time' => now(),
+        ]);
 
         return redirect()->back()->with('success_count', 'Number of Calls Updated Successfully.');
     }
