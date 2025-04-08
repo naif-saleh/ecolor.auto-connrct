60a99e59e6e2462b88a39f4755d53163ded91ac5
naif-saleh <naif.saleh.code@gmail.com>
2025-04-08 11:02:50 +0200

    Handle Dist Calls

--- routes/console.php
+++ routes/console.php
@@ -34,7 +34,7 @@ Schedule::command('app:ADist-make-call-command')->everySecond();
 
 // // Call Status Commands
 Schedule::command('app:ADial-participants-command')->everySecond();
-//Schedule::command('app:ADist-participants-command')->everySecond();
+Schedule::command('app:ADist-participants-command')->everySecond();
 
 // // Update User Status Commands
 Schedule::command('app:ADist-update-user-status-command')->everySecond();
