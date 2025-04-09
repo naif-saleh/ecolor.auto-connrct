36d79a5bd02b95b4c5235df79a57fe529721015c
naif-saleh <naif.saleh.code@gmail.com>
2025-04-09 14:11:16 +0200

    Handle Dist Calls

--- app/Models/ADistAgent.php
+++ app/Models/ADistAgent.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class ADistAgent extends Model
 {
 
+    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
