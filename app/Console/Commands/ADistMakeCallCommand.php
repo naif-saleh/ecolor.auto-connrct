735fef5cd145f7bc8b760c8e7181f38d01134a61
naif-saleh <naif.saleh.code@gmail.com>
2025-04-13 12:39:53 +0200

    handl UbdateUserCommand to getUsers from ThreeCxServices Class

--- app/Console/Commands/ADistMakeCallCommand.php
+++ app/Console/Commands/ADistMakeCallCommand.php
@@ -34,7 +34,7 @@ class ADistMakeCallCommand extends Command
         Log::info("Using timezone: {$timezone}");
 
         $agents = ADistAgent::whereHas('files', function ($query) {
-            $query->where('is_done', '!=' , 'called')->where('allow', true)->where('date', Carbon::today());
+            $query->where('is_done', '!=' , 'called')->where('allow', true)->where('date', Carbon::today()->format('Y-m-d'));
         })->get();
 
         Log::info('Agents query executed.', ['agents_count' => $agents->count()]);
